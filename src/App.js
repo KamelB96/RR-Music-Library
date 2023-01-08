674b2ef3aff015422ff90218eff309134a8390c1
kamel Barakat <kamelbarakat96@yahoo.com>
2023-01-08 18:02:50 -0500

    rr-6-improve-a-music-library-interface

--- src/App.js
+++ src/App.js
@@ -7,52 +7,50 @@ import AlbumView from './components/AlbumView'
 import ArtistView from './components/ArtistView'
 
 function App() {
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
-
+    let [data, setData] = useState([]);
+    let [message, setMessage] = useState("Search for Music!");
+    let [search, setSearch] = useState('');
+  
     const API_URL = 'https://itunes.apple.com/search?term='
-
-    useEffect(() => {
-        if(search) {
-            const fetchData = async () => {
-                document.title = `${search} Music`
-                const response = await fetch(API_URL + search)
-                const resData = await response.json()
-                if (resData.results.length > 0) {
-                    return setData(resData.results)
-                } else {
-                    return setMessage('Not Found')
-                }
-            }
-            fetchData()
+  
+    useEffect(()=>{
+      if(search) {
+        const fetchData = async () => {
+          document.title = `${search} Music`
+        const response = await fetch(API_URL + search)
+        const resData = await response.json();
+          if (resData.results.length > 0) {
+            setData(resData.results);
+          } else {
+            setMessage("Not Found");
+          }
         }
-    }, [search])
-    
+        fetchData();
+      }
+    },[search]);
+  
     const handleSearch = (e, term) => {
-        e.preventDefault()
-        setSearch(term)
+      e.preventDefault()
+      setSearch(term)
     }
-
+  
     return (
-      <div>
-      {message}
-          <Router>
-              <Routes>
-                  <Route path="/" element={
-                      <Fragment>
-                          <SearchBar handleSearch = {handleSearch}/>
-                          <Gallery data={data} />
-                      </Fragment>
-                  } />
-                  <Route path="/album/:id" element={<AlbumView />} />
-                  <Route path="/artist/:id" element={<ArtistView />} />
-              </Routes>
-          </Router>
+      <div className='App'>      
+        {message}
+        <Router>
+          <Routes>
+            <Route path='/' element= {
+              <Fragment>
+                <SearchBar handleSearch = {handleSearch} />
+                <Gallery data={data} />
+              </Fragment>
+            }/>
+            <Route path='/album/:id' element={<AlbumView/>} />
+            <Route path='/artist/:id' element={<ArtistView/>} />
+          </Routes>
+        </Router>
       </div>
-  )
-  ;
-}
-
-export default App;
-
+    );
+  }
+  
+  export default App;
--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,18 +1,30 @@
-import { useState } from 'react'
+import { useState } from 'react' 
 import { Link } from 'react-router-dom'
 
-function GalleryItem(props) {
+function GalleryItem(props){
     let [view, setView] = useState(false)
+
+    const simpleStyle = {
+        'width': '25vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px'
+    }
+    
+    const detailStyle = {
+        'width': '80vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'color': 'yellow'
+    }
 
     const simpleView = () => {
         return (
-            <div style={{
-                'width': '25vw',
-                'height': '20vh',
-                'border': '1px solid black',
-                'margin': '2px',
-                'position': 'relative'
-            }}>
+            <div style={simpleStyle}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -21,17 +33,7 @@ function GalleryItem(props) {
 
     const detailView = () => {
         return (
-            <div style={{
-                'width': '80vw',
-                'height': '20vh',
-                'border': '1px solid black',
-                'margin': '2px',
-                'position': 'relative',
-                'backgroundImage': `url(${props.item.artworkUrl100})`,
-                'backgroundRepeat': 'no-repeat',
-                'backgroundSize': 'cover',
-                'color': 'white'
-            }}>
+            <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>
                     <Link to={`/artist/${props.item.artistId}`}>
@@ -45,15 +47,13 @@ function GalleryItem(props) {
                 </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
-
             </div>
-
         )
     }
 
     return (
-        <div onClick={() => setView(!view)}
-            style={{ 'display': 'inline-block' }}>
+        <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
+            {/* This simple ternary shows the simple view when 'view' is false! */}
             {view ? detailView() : simpleView()}
         </div>
     )
