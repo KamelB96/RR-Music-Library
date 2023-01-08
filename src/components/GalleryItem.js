0ef306a9e4fec7a02403481b79e7cfbbc1e0beef
kamel Barakat <kamelbarakat96@yahoo.com>
2023-01-08 12:19:33 -0500

    With Router Branch complete

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-function GalleryItem(props){
+function GalleryItem(props) {
     let [view, setView] = useState(false)
 
     const simpleView = () => {
@@ -9,7 +10,7 @@ function GalleryItem(props){
                 'width': '25vw',
                 'height': '20vh',
                 'border': '1px solid black',
-                'margin' : '2px',
+                'margin': '2px',
                 'position': 'relative'
             }}>
                 <h3>{props.item.trackName}</h3>
@@ -24,7 +25,7 @@ function GalleryItem(props){
                 'width': '80vw',
                 'height': '20vh',
                 'border': '1px solid black',
-                'margin' : '2px',
+                'margin': '2px',
                 'position': 'relative',
                 'backgroundImage': `url(${props.item.artworkUrl100})`,
                 'backgroundRepeat': 'no-repeat',
@@ -32,19 +33,30 @@ function GalleryItem(props){
                 'color': 'white'
             }}>
                 <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
+                <h3>
+                    <Link to={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </Link>
+                </h3>
+                <h3>
+                    <Link to={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </Link>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
+
             </div>
+
         )
     }
 
     return (
         <div onClick={() => setView(!view)}
-        style={{'display': 'inline-block'}}>
+            style={{ 'display': 'inline-block' }}>
             {view ? detailView() : simpleView()}
         </div>
     )
-} 
+}
 
 export default GalleryItem
