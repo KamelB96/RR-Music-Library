ea6a2cbbc7f38ac0e8280de82aba271c1a5fdd87
kamel Barakat <kamelbarakat96@yahoo.com>
2023-01-09 11:20:56 -0500

    Added suspense and loading spinner

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -3,15 +3,26 @@ import { useState } from 'react'
 function GalleryItem(props){
     let [view, setView] = useState(false)
 
+    const simpleStyle = {
+        'width': '25vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px'
+    }
+    const detailStyle = {
+        'width': '80vw',
+        'height': '20vh',
+        'border': '1px solid black',
+        'margin': '2px',
+        'backgroundImage': `url(${props.item.artworkUrl100})`,
+        'backgroundRepeat': 'no-repeat',
+        'backgroundSize': 'cover',
+        'color': 'yellow'
+    }
+
     const simpleView = () => {
-        return (
-            <div style={{
-                'width': '25vw',
-                'height': '20vh',
-                'border': '1px solid black',
-                'margin' : '2px',
-                'position': 'relative'
-            }}>
+        return(
+            <div style={simpleStyle}>
                 <h3>{props.item.trackName}</h3>
                 <h4>{props.item.collectionName}</h4>
             </div>
@@ -20,17 +31,7 @@ function GalleryItem(props){
 
     const detailView = () => {
         return (
-            <div style={{
-                'width': '80vw',
-                'height': '20vh',
-                'border': '1px solid black',
-                'margin' : '2px',
-                'position': 'relative',
-                'backgroundImage': `url(${props.item.artworkUrl100})`,
-                'backgroundRepeat': 'no-repeat',
-                'backgroundSize': 'cover',
-                'color': 'white'
-            }}>
+            <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
                 <h3>{props.item.collectionName}</h3>
                 <h4>{props.item.primaryGenreName}</h4>
@@ -39,12 +40,13 @@ function GalleryItem(props){
         )
     }
 
-    return (
-        <div onClick={() => setView(!view)}
-        style={{'display': 'inline-block'}}>
+    return(
+        <div onClick={() => setView(!view)} style={{'display': 'inline-block'}}>
+            <p>One Gallery Item</p>
             {view ? detailView() : simpleView()}
         </div>
     )
-} 
+
+}
 
 export default GalleryItem
